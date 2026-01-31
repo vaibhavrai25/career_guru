ab2a64c38acbf64a2ce5368e0b77657ac251969a
PURUSHOTTAM06 <purushottam.2006.ms@gmail.com>
2026-02-01 01:40:58 +0530

    Add advanced analytics engines: streak, trend, topics, persona

--- backend/src/routes/statsRoutes.js
+++ backend/src/routes/statsRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { syncCodeforces } = require('../controllers/statsController');
+const { syncCodeforces, syncLeetCode,  getOverviewStats ,getConsistencyStats
+    , getDifficultyTrend ,  getTopicStrength, getCodingPersona} = require('../controllers/statsController');
+
 const { protect } = require('../middlewares/authMiddleware');
 
 router.get('/sync', protect, syncCodeforces);
+router.get('/sync/leetcode', protect, syncLeetCode);
+router.get('/overview', protect, getOverviewStats);
+router.get('/consistency', protect, getConsistencyStats);
+router.get('/difficulty-trend', protect, getDifficultyTrend);
+router.get('/topic-strength', protect, getTopicStrength);
+router.get('/coding-persona', protect, getCodingPersona);
+
+
 
 module.exports = router;
